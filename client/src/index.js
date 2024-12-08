deacb4d4dd13b8dcff73c73acae9cfc015289db4
anthonyfullstack <anthony.madlambayan@utas.edu.om>
2024-12-08 15:09:23 +0400

    first upload commit

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { store } from "./Store/Store";
+import { Provider } from "react-redux";
+//import Register from "./Components/Register";
+//import Login from "./Components/Login";
+
+import { persistore } from "./Store/Store";
+import { PersistGate } from "redux-persist/integration/react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <PersistGate loading={null} persistor={persistore}>
+        <App />
+      </PersistGate>
+    </React.StrictMode>
+  </Provider>
+);
